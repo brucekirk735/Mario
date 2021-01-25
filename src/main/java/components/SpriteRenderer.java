1fcffd280f394d9654fab93a923a69e1df42e054
Bruce <brucekirk@gmail.com>
2021-01-25 16:37:47 -0500

    Chapter 16 zindexing

--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -14,30 +14,29 @@ public class SpriteRenderer extends Component {
     private Transform lastTransform;
     private boolean isDirty = false;
 
-
     public SpriteRenderer(Vector4f color) {
         this.color = color;
         this.sprite = new Sprite(null);
+        this.isDirty = true;
     }
 
     public SpriteRenderer(Sprite sprite) {
         this.sprite = sprite;
         this.color = new Vector4f(1, 1, 1, 1);
+        this.isDirty = true;
     }
 
     @Override
     public void start() {
-
         this.lastTransform = gameObject.transform.copy();
     }
 
     @Override
     public void update(float dt) {
-        if (!this.lastTransform.equals(this.gameObject.transform)){
+        if (!this.lastTransform.equals(this.gameObject.transform)) {
             this.gameObject.transform.copy(this.lastTransform);
             isDirty = true;
         }
-
     }
 
     public Vector4f getColor() {
@@ -52,23 +51,23 @@ public class SpriteRenderer extends Component {
         return sprite.getTexCoords();
     }
 
-    public void setSprite (Sprite sprite) {
+    public void setSprite(Sprite sprite) {
         this.sprite = sprite;
         this.isDirty = true;
     }
-    public void setColor(Vector4f color){
+
+    public void setColor(Vector4f color) {
         if (!this.color.equals(color)) {
             this.isDirty = true;
-            this.color.set(color) ;
-
-
+            this.color.set(color);
         }
     }
-    public  boolean isDirty(){
+
+    public boolean isDirty() {
         return this.isDirty;
     }
 
-    public  void setClean (){
-        this.isDirty= false;
+    public void setClean() {
+        this.isDirty = false;
     }
 }
