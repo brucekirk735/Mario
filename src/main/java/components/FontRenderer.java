aed49ef00eee37b30aece49e6d21a2a0e5c55789
brucekirk735 <37710779+brucekirk735@users.noreply.github.com>
2021-04-10 02:05:16 -0400

    Add files via upload

--- src/main/java/components/FontRenderer.java
+++ src/main/java/components/FontRenderer.java
@@ -1,16 +1,11 @@
 package components;
 
-import jade.Component;
-
-import java.awt.font.FontRenderContext;
-
 public class FontRenderer extends Component {
 
     @Override
     public void start() {
         if (gameObject.getComponent(SpriteRenderer.class) != null) {
             System.out.println("Found Font Renderer!");
-
         }
     }
 
